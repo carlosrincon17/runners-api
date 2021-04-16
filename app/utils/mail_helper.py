e1359974982d0de5473890b90d4d7a6b2c3c4438
Carlos Rincón <carlosjrinconavila@gmail.com>
2021-04-15 21:30:15 -0500

    Added email notifications

--- app/utils/mail_helper.py
+++ app/utils/mail_helper.py
@@ -1,4 +1,7 @@
+from email.mime.text import MIMEText
+
 from fastapi_mail import ConnectionConfig, MessageSchema, FastMail
+from jinja2 import Environment, PackageLoader, select_autoescape
 
 from app.settings import mail_setting
 
@@ -16,67 +19,38 @@ conf = ConnectionConfig(
 class MailHelper:
 
     async def send_welcome_email(self, registration_data, user_email):
-        welcome_email_template = """
-        <p>
-            Hola {first_name},
-        </p>
-        <p>
-            Runnerscucuta te da la bienvenida a esta gran fiesta running. Te confirmamos el 
-            registro de tus datos correctamente, solo te falta un paso, registra el 
-            soporte del pago y te haremos llegar la confirmación final de la inscripción. 
-        </p>
-        <p> Estos son tus datos de inscripción: </p>
-        - Nombres: {full_name} <br>
-        - Télefono: {phone} <br>
-        - Dirección: {address} <br>
-        - Identificación: {document_number} <br>
-        <p>
-            Somos un equipo de running aficionado y recreativo, que fomenta hábitos de vida saludable e 
-            impulsa la cultura running en nuestra región. <br />
-            Nuestro lema: <strong>#YOCORROPORSALUD. </strong> <br />
-            Te invitamos a que revises nuestras redes sociales instagram y Facebook @runnerscucuta y Runnerscucuta.com
-        </p>
-        """.format(**registration_data)
         await self.send_email(
-            subject='Inscripción a II Carrera Virtual Runner Cúcuta',
-            html_template=welcome_email_template,
-            recipents=[user_email]
+            subject='Inscripción a II Carrera Virtual Runners Cúcuta',
+            html_template="",
+            recipents=[user_email],
+            template_name='welcome.html',
+            template_data=registration_data,
         )
 
     async def send_registration_email(self, registration_data, user_email):
-        welcome_email_template = """
-        <p>
-            Hola {first_name},
-        </p>
-        <p>
-            Runnerscucuta <strong>TE CONFIRMA</strong> que tu registro de datos personales y pago de la inscripción fue 
-            exitosa y nuevamente te damos la bienvenida a esta gran fiesta running y 
-            deseamos que sea tu mejor experiencia.
-        </p>
-        <p>
-            Te invitamos a seguir generando cultura running y hábitos de vida saludable con los que te rodean 
-            y recuerda  #YOCORROPORSALUD.
-        </p>
-        <p>
-            Somos un equipo de running aficionado y recreativo, que fomenta hábitos de vida saludable e 
-            impulsa la cultura running en nuestra región. <br />
-            Nuestro lema: <strong> #YOCORROPORSALUD. </strong> <br />
-            Te invitamos a que revises nuestras redes sociales instagram y Facebook @runnerscucuta y Runnerscucuta.com
-        </p>
-        """.format(**registration_data)
         await self.send_email(
-            subject='Confirmación de inscripción a II Carrera Virtual Runner Cúcuta',
-            html_template=welcome_email_template,
-            recipents=[user_email]
+            subject='Confirmación de inscripción a II Carrera Virtual Runners Cúcuta',
+            html_template="",
+            recipents=[user_email],
+            template_name='confirmed_payment.html',
+            template_data=registration_data,
         )
 
     @classmethod
-    async def send_email(cls, subject, html_template, recipents):
+    async def send_email(cls, subject, html_template, recipents, template_name=None, template_data=None):
+        if template_name:
+            env = Environment(
+                loader=PackageLoader('app', 'templates/email'),
+                autoescape=select_autoescape(['html', 'xml', 'txt'])
+            )
+            template = env.get_template(template_name)
+            html_template = template.render(**template_data)
+
         message = MessageSchema(
             subject=subject,
-            recipients=recipents,  # List of recipients, as many as you can pass
-            body=html_template,
-            subtype="html"
+            recipients=recipents,
+            html=html_template.encode('utf-8'),
+            subtype="html",
         )
         fm = FastMail(conf)
         await fm.send_message(message)
